2b6df980b526d0ed4140e498750a1151a18ed86d
Rajasekhar.k1223 <rajasekhar.k1223@gmail.com>
2025-06-13 17:06:13 +0530

    13-06-2025_commi

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: str
     username: str
