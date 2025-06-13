6ad00309469226b255a65e9f9561bf4a97d3e7fd
Rajasekhar.k1223 <rajasekhar.k1223@gmail.com>
2025-06-13 15:51:39 +0530

    13-06-2025_commit

--- app/db/postgresql.py
+++ app/db/postgresql.py
@@ -1,9 +1,10 @@
 from sqlmodel import create_engine, Session
 from app.models.user_model import User
 
-DATABASE_URL = "postgresql+asyncpg://user:pass@localhost/dbname"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "postgresql://postgres:pnQgJJPXsAUwhNMKpfthUwhmxOTovupn@caboose.proxy.rlwy.net:35090/railway"
 
-async def get_db():
+engine = create_engine(DATABASE_URL, echo=True)  # echo=True for SQL logs (optional)
+
+def get_db():
     with Session(engine) as session:
         yield session
